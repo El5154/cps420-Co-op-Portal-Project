972fa72d0fa70289f240dd239774b8d78c06b00f
El5154 <65725962+El5154@users.noreply.github.com>
2026-03-30 14:29:09 -0400

    added a frontend way for the supervisor to upload a file for evaluation

--- docs/evaluation.js
+++ docs/evaluation.js
@@ -12,6 +12,7 @@ const evaluationStatusSpan = document.getElementById("evaluationStatus");
 const supervisorNameSpan = document.getElementById("supervisorName");
 const supervisorEmailSpan = document.getElementById("supervisorEmail");
 const performanceSpan = document.getElementById("overallPerformance");
+const evaluationFileSpan = document.getElementById("evaluationFile");
 
 function showMessage(text, type) {
   message.textContent = text;
@@ -89,5 +90,43 @@ submitBtn.addEventListener("click", async () => {
   } catch (error) {
     showMessage("Could not connect to the server.", "error");
   }
+});
+
+uploadBtn.addEventListener("click", async () => {
+    const file = evaluationFileSpan.files[0];
+
+    if (!file) {
+        return showMessage("Please select a file to upload.", "error");
+    }
+
+    if (file.size === 0) {
+        return showMessage("Cannot upload an empty file.", "error");
+    }
+
+    if (file.type !== "application/pdf") {
+        return showMessage("Only PDF files are allowed.", "error");
+    }
+
+    const formData = new FormData();
+    formData.append("evaluationFile", file);
+
+    try {
+        const reponse = await fetch(`${BASE_URL}/uploadEvaluationFile`, {
+            method: "POST",
+            credentials: "include",
+            body: formData
+        });
+
+        const data = await response.json();
+
+        if (!response.ok) {
+            return showMessage(data.error || "Failed to upload evaluation file.", "error");
+        }
+
+        showUploadMessage(data.message || "Evaluation file uploaded successfully.", "success");
+        evaluationFileInput.value = "";
+    } catch (error) {
+        showMessage("Could not connect to the server.", "error");
+    }
 });
 
